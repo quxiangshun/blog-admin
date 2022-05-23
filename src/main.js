015bce337ac99dfd86681490ac0f55232d65e941
shun <quxiangshun@gmail.com>
2022-05-23 15:46:16 +0800

    feat: 权限校验，注册全局指令Permission

--- src/main.js
+++ src/main.js
@@ -15,6 +15,10 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+// 导入指令
+import directive from '@/directive'
+Vue.use(directive)
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
